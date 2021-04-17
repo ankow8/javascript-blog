58654a93cfd8b44baa8c2713b11b644c725f8144
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-04-17 23:25:55 +0200

    Add two remove function in js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,21 @@ const titleClickHandler = function(event){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
