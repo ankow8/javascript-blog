86c32d7512fd119feb93cdcac112e5c2bd40a267
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-04-19 22:56:12 +0200

    Add 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@
 });*/
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,15 +17,13 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
-
-  const clickedElement = this;
-
-  console.log('clickedElement:', clickedElement);
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -34,6 +32,10 @@ const titleClickHandler = function(event){
   }
 
   /* get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+
+  console.log('articleSelector: ', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
