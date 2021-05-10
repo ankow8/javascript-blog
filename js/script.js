8644c1c073055200530192f3208b0060b037bd9b
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-05-10 16:28:31 +0200

    Add calculateTagsParams

--- js/script.js
+++ js/script.js
@@ -96,6 +96,23 @@ function generateTitleLinks(customSelector = ''){
 
 generateTitleLinks();
 
+const calculateTagsParams = function(tags){
+  const params = {
+    max: 0,
+    min: 999999,
+  };
+  for(let tag in tags){
+    console.log(tag + ' is used ' + tags[tag] + ' times');
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+    if(tags[tag] > params.min){
+      params.min = tags[tag];
+    }
+  }
+  return params;
+};
+
 function generateTags(){
 
   /* [NEW] create a new variable allTags with an empty object */
@@ -154,6 +171,8 @@ function generateTags(){
   //console.log('allTags: ', tagList);
 
   /* [NEW] create variable for all links HTML code */
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams: ', tagsParams);
   let allTagsHTML = '';
 
   /* [NEW] START LOOP: for each tag in allTags: */
