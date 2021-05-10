1f6381ad5424ddc9f51ad06a295cec76d73ac6f5
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-05-10 15:59:09 +0200

    Create tag list and links in right sidebar

--- js/script.js
+++ js/script.js
@@ -99,8 +99,7 @@ generateTitleLinks();
 function generateTags(){
 
   /* [NEW] create a new variable allTags with an empty object */
-  let allTags = [];
-
+  let allTags = {};
 
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
@@ -130,10 +129,12 @@ function generateTags(){
       html = html + tagHTML;
 
       /* [NEW] check if this link is NOT already in allTags  */
-      if(allTags.indexOf(tagHTML) == -1){
-        /* [NEW] add generated code to allTags array */
-        allTags.push(tagHTML);
-        console.log('allTags: ', allTags);
+      if(!allTags[tag]){
+        /* [NEW] add generated code to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
 
     /* END LOOP: for each tag */
     }
@@ -149,7 +150,24 @@ function generateTags(){
   const tagList = document.querySelector('.tags');
 
   /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
+  //tagList.innerHTML = allTags;
+  //console.log('allTags: ', tagList);
+
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+  for(let tag in allTags){
+
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ') ' + '</a></li>';
+
+  /* [NEW] END LOOP: for each tag in allTags: */
+  }
+
+  /*[NEW] add HTML from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
+
 }
 
 generateTags();
