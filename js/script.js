b86c9a2ca8d24c61278ca9b243beec1350ae4c56
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-05-10 13:50:48 +0200

    Add taggs in right side

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = ''){
 
@@ -97,6 +98,10 @@ generateTitleLinks();
 
 function generateTags(){
 
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = [];
+
+
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
@@ -124,6 +129,12 @@ function generateTags(){
       /* add generated code to html variable */
       html = html + tagHTML;
 
+      /* [NEW] check if this link is NOT already in allTags  */
+      if(allTags.indexOf(tagHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(tagHTML);
+        console.log('allTags: ', allTags);
+
     /* END LOOP: for each tag */
     }
 
@@ -133,6 +144,12 @@ function generateTags(){
 
   /* END LOOP: for every article: */
   }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 
 generateTags();
